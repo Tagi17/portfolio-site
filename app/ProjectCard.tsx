ab61a62466c2adbfa3100c4b73769052aae0fc7b
Nazia <naziaozair@gmail.com>
2024-03-06 23:16:42 -0500

    working on implementing horizontal scrolling within a card section

--- app/ProjectCard.tsx
+++ app/ProjectCard.tsx
@@ -23,82 +23,65 @@ const ProjectCard = () => {
   const progressWrap = gsap.utils.wrap(0, 1);
 
   useGSAP(() => {
-    const slides = gsap.utils.toArray(".slide .objects, .slide .image");
-    const sections = gsap.utils.toArray(".section2");
-    // const buttons = gsap.utils.toArray('.section2');
-    gsap.set(slides, { autoAlpha: 0 });
-    const changeItems = gsap.utils.toArray(".objects .image") as HTMLElement[];
+    let sectionItems = document.querySelector(".main-slide") as HTMLElement;
+    let tween: GSAPAnimation | undefined; 
+    let panels = gsap.utils.toArray(".main-slide .slide-item .item");
+    const width = window.innerHeight * (panels.length - 1);
+    const links = gsap.utils.toArray(".navTitle") as HTMLElement[];
+    const modifiedLength = links.length - 1;
+    let activeLink = 0;
+    const snapPoints = links.map((_, i) => i / modifiedLength);
+    const mySnap = gsap.utils.snap(snapPoints);
+    links[activeLink].classList.toggle("active");
+    gsap.set(".slide", { height: "0" });
 
-    // const scrollTween = gsap.to(changeItems, {
-    //   xPercent: -100 * (changeItems.length - 1),
-    //   ease: "none",
-    //   scrollTrigger:{
-    //     trigger: ".slides-container",
-    //     toggleActions: 'restart none resume reset',
-    //     scrub: 0.1,
-    //     markers: true
-    //   },
-    // });
-
-    // buttons.forEach((button, i) => {
-    //   ScrollTrigger.create({
-    //     trigger: button,
-    //     start: "top center", // Adjust these values based on your layout
-    //     onEnter: () => showContent(i),
-    //     onEnterBack: () => showContent(i),
-    //   });
-    // });
-    // gsap.set(".section2:not(:first-child) .objects", { height: "0" });
-    const slidesContainer = document.querySelector(".slides-container") as HTMLElement | null;
-    if (!slidesContainer) {
-      throw new Error("Slides container not found");
+    function setupScrollTweens() {
+      tween = gsap.to(panels, {
+        xPercent: -100 * (panels.length - 1),
+        ease: "none",
+        scrollTrigger: {
+          trigger: ".main-slide",
+          pin: true,
+          scrub: 0.1,
+          end: `+=1000`, 
+          onUpdate: (self) => {
+            const newIndex = mySnap(self.progress) * modifiedLength;
+            if (newIndex !== activeLink) {
+              links[activeLink].classList.toggle("active");
+              links[newIndex].classList.toggle("active");
+              activeLink = newIndex;
+            }
+          }
+        }
+      });
     }
-    const endValue1 = "+=" + slidesContainer.offsetWidth;
-    const scrollTween = gsap.to(sections, {
-      xPercent: -100 * (sections.length - 1),
-      ease: "none",
-      scrollTrigger: {
-        trigger: ".slides-container",
-        toggleActions: "restart none resume reset",
-        pin: true,
-        scrub: 0.1,
-        markers: true,
-        end: () => endValue1,
-      },
-    });
-    function updateSlideContent(index: number) {
-      gsap.set(".slide > div", { autoAlpha: 0 });
-      gsap.to(".slide .objects:nth-of-type(" + (index + 1) + ")", {
-        autoAlpha: 1,
-        duration: 0.5,
-      });
-      sections.forEach((section, i) => {
-        const element = section as HTMLElement;
-        ScrollTrigger.create({
-          trigger: element,
-          start: "left center",
-          end: "right center",
-          containerAnimation: scrollTween,
-          onEnter: () => updateSlideContent(i),
-          onEnterBack: () => updateSlideContent(i),
-        });
-      });
-      // gsap.to(".slide .image:nth-of-type(" + (index + 1) + ")", {
-      //   autoAlpha: 1,
-      //   duration: 0.5,
-      // });
-    }
+    setupScrollTweens();
 
-    changeItems.forEach((section2, i) => {
-      gsap.to(".section2", {
-        x: 100,
-        opacity: 0,
+    document.querySelectorAll(".navTitle").forEach((anchor) => {
+      anchor.addEventListener("click", function (e) {
+        e.preventDefault();
+        const target = e.currentTarget as HTMLElement; 
+        const targetHref = target.getAttribute("href");
+        if (targetHref) {
+          let targetElem = document.querySelector(targetHref) as HTMLElement;
+          if (targetElem) {
+            if (tween && 'scrollTrigger' in tween && tween.scrollTrigger) {
+              const range = gsap.utils.mapRange(0, width, 0, tween.scrollTrigger.end);
+              const yPos = range(targetElem.offsetLeft);
+              gsap.to(window, {
+                scrollTo: {
+                  y: yPos,
+                  autoKill: false
+                },
+                duration: 0.5
+              });
+            }
+          }
+        }
       });
-      // if (changeItems[i + 1]) {
-      //   tlChange.to(section2, { height: 0 }).to(changeItems[i + 1], { height: "auto" }, "<");
-      // }
     });
-
+    
+  
     const items = gsap.utils.toArray(".item1 .content1") as HTMLElement[];
 
     const endValue = items.reduce(
@@ -109,13 +92,13 @@ const ProjectCard = () => {
     const tl = gsap.timeline({
       scrollTrigger: {
         trigger: ".accordion-section1",
-        start: "top 30%",
+        start: "top 40%",
         // end: "+=" + 100 * items.length + "%",
         end: "+=600",
         // pin: false,
         scrub: true,
         invalidateOnRefresh: true,
-        // markers: true,
+        markers: true,
       },
     });
 
@@ -137,87 +120,122 @@ const ProjectCard = () => {
   return (
     <div>
       {/* <Parallax speed={1}> */}
-      <div className="section1 accordion-section1 flex justify-center items-center min-h-screen h-screen">
-        <div className="wrapper1 w-full px-4 md:px-20 max-w-screen-xl mx-auto">
-          <div className="item1 mb-12 border-2 border-primary-300 bg-primary-500 rounded-lg shadow-xl overflow-hidden  ">
-            <div className="header1 bg-primary-600 text-white text-xl font-bold p-5 cursor-pointer">
-              <div className="three-d text-5xl font-bold p-3">About Me</div>
-            </div>
-            <div className="content1 three-d text-3xl px-14 py-9">
-              <div className="">
-                Fuelled by a passion for crafting impactful products, my journey
-                into programming began with Girls Who Code—an experience that
-                sparked my curiosity in tech. It wasn&apos;t until I encountered
-                Bitcoin that I found my true calling. The technology behind it
-                not only captivated me but also unveiled a realm of exciting
-                possibilities. Now, I&apos;m integrating blockchain into my
-                projects and I leverage my design skills to enhance user
-                experiences in innovative ways.
+        <div className="section1 accordion-section1 flex justify-center items-center min-h-screen h-screen">
+          <div className="wrapper1 w-full px-4 md:px-20 max-w-screen-xl mx-auto">
+            <div
+              className="item1 mb-12 border-2 border-primary-300 bg-primary-500 rounded-lg shadow-xl overflow-hidden"
+              id="about-me"
+              data-label="label0"
+            >
+              <div className="header1 bg-primary-600 text-white text-xl font-bold p-5 cursor-pointer">
+                <div className="three-d text-5xl font-bold p-3">About Me</div>
               </div>
-              <div className="p-5"></div>
-            </div>
-          </div>
-          <div className="item1 mb-12 border-2 border-primary-300 bg-primary-500 rounded-lg shadow-xl overflow-hidden">
-            <div className="header1 bg-primary-600 text-white text-xl font-bold p-4 cursor-pointer">
-              <div className="three-d text-5xl font-bold p-3">Projects</div>
-            </div>
-            <div className="content1 three-d text-3xl px-14 py-9">
-              <div>
-                Currently working on the smart-dashboard, churro-relay, and The
-                League
-              </div>
-              <div className="p-5"></div>
-              <div className="slides-container">
-                <div className="sections-inner">
-                  <div className="section2" id="button1"></div>
-                  <div className="section2" id="button2"></div>
-                  <div className="section2" id="button3"></div>
-                  <div className="section2" id="button4"></div>
-                </div>
-              </div>
-              <div className="slide">
+              <div className="content1 three-d text-3xl px-14 py-9">
                 <div className="">
-                  <div className="objects">Relay Project is about</div>
-                  <div className="image">
-                    <Image src={defi} height={50} width={50} alt={"image"} />
+                  Fuelled by a passion for crafting impactful products, my
+                  journey into programming began with Girls Who Code—an
+                  experience that sparked my curiosity in tech. It wasn&apos;t
+                  until I encountered Bitcoin that I found my true calling. The
+                  technology behind it not only captivated me but also unveiled
+                  a realm of exciting possibilities. Now, I&apos;m integrating
+                  blockchain into my projects and I leverage my design skills to
+                  enhance user experiences in innovative ways.
+                </div>
+                <div className="p-5"></div>
+              </div>
+            </div>
+            <div
+              className="item1 mb-12 border-2 border-primary-300 bg-primary-500 rounded-lg shadow-xl overflow-hidden"
+              id="projects"
+              data-label="label1"
+            >
+              <div className="header1 bg-primary-600 text-white text-xl font-bold p-4 cursor-pointer">
+                <div className="three-d text-5xl font-bold p-3">Projects</div>
+              </div>
+              <div className="content1 three-d text-3xl px-14 py-9">
+                <div>
+                  Currently working on the smart-dashboard, churro-relay, and
+                  The League
+                </div>
+                <div className="p-5"></div>
+                <div className="main-slide">
+                  <div className="slides-container">
+                    <div className="navTitle-wrap">
+                      <div className="sections-inner">
+                        <div className="slide-item link-1">
+                          <div className="section2 navTitle" id="button1"></div>
+                          <a href="item1" className="section2 navTitle"></a>
+                        </div>
+                        <div className="slide-item link-2">
+                          <div className="section2 navTitle" id="button2"></div>
+                          <a href="item2" className="section2 navTitle"></a>
+                        </div>
+                        <div className="slide-item link-3">
+                          <div className="section2 navTitle" id="button3"></div>
+                          <a href="item3" className="section2 navTitle"></a>
+                        </div>
+                        <div className="slide-item link-4">
+                          <div className="section2 navTitle" id="button4"></div>
+                          <a href="item4" className="section2 navTitle"></a>
+                        </div>
+                      </div>
+                    </div>
+                </div>
+                <div className="slide">
+                  <div className="item item-1">
+                    <div className="objects">Relay Project is about</div>
+                    <div className="image">
+                      <Image src={defi} height={50} width={50} alt={"image"} />
+                    </div>
                   </div>
-                  <div className="objects">Relay Project is about</div>
-                  <div className="image">
-                    <Image src={defi} height={50} width={50} alt={"image"} />
+                  <div className="item item-2">
+                    <div className="objects">Relay Project is about</div>
+                    <div className="image">
+                      <Image src={defi} height={50} width={50} alt={"image"} />
+                    </div>
                   </div>
-                  <div className="objects">Relay Project is about</div>
-                  <div className="image">
-                    <Image src={defi} height={50} width={50} alt={"image"} />
+                  <div className="item item-3">
+                    <div className="objects">Relay Project is about</div>
+                    <div className="image">
+                      <Image src={defi} height={50} width={50} alt={"image"} />
+                    </div>
                   </div>
-                  <div className="objects">Relay Project is about</div>
-                  <div className="image">
-                    <Image src={defi} height={50} width={50} alt={"image"} />
+                  <div className="item item-4">
+                    <div className="objects">Relay Project is about</div>
+                    <div className="image">
+                      <Image src={defi} height={50} width={50} alt={"image"} />
+                    </div>
                   </div>
                 </div>
+                <div className="p-1"></div>
               </div>
-              <div className="p-1"></div>
             </div>
-          </div>
-          <div className="item2 mb-12 border-2 border-primary-300 bg-primary-500 rounded-lg shadow-xl overflow-hidden">
-            <div className="header1 bg-primary-600 text-white text-xl font-bold p-4 cursor-pointer">
-              <div className="three-d text-5xl font-bold p-2">Boop</div>
-            </div>
-            <div className="content1 three-d text-2xl p-4">
-              <div>
-                Libero assumenda iste, tempora eos facilis consectetur cum esse
-                officia magnam repudiandae mollitia itaque voluptates corporis
-                eveniet perferendis saepe hic aliquid veritatis ipsa similique
-                voluptatibus nisi harum. Voluptates blanditiis nam, aliquid
-                distinctio repellendus molestiae numquam doloremque totam
-                impedit. Recusandae laborum debitis quidem assumenda laudantium
-                perferendis reprehenderit repellendus quos, labore ratione,
-                laboriosam autem!
+            <div
+              className="item1 mb-12 border-2 border-primary-300 bg-primary-500 rounded-lg shadow-xl overflow-hidden"
+              id="contact"
+              data-label="label2"
+            >
+              <div className="header1 bg-primary-600 text-white text-xl font-bold p-4 cursor-pointer">
+                <div className="three-d text-5xl font-bold p-2">Boop</div>
+              </div>
+              <div className="content1 three-d text-2xl px-8 py-8">
+                <div>
+                  Libero assumenda iste, tempora eos facilis consectetur cum
+                  esse officia magnam repudiandae mollitia itaque voluptates
+                  corporis eveniet perferendis saepe hic aliquid veritatis ipsa
+                  similique voluptatibus nisi harum. Voluptates blanditiis nam,
+                  aliquid distinctio repellendus molestiae numquam doloremque
+                  totam impedit. Recusandae laborum debitis quidem assumenda
+                  laudantium perferendis reprehenderit repellendus quos, labore
+                  ratione, laboriosam autem!
+                </div>
+                <div className="p-5"></div>
               </div>
             </div>
           </div>
         </div>
+        {/* </Parallax> */}
       </div>
-      {/* </Parallax> */}
     </div>
   );
 };
--- app/navbar.tsx
+++ app/navbar.tsx
@@ -9,21 +9,21 @@ export default function Navbar() {
     <div>
         <nav className="mx-auto mt-10 p-2 border-2 border-primary-300 bg-primary-500 rounded-lg shadow-lg w-7/12 flex items-center justify-between">
             <div className="flex items-center">
-              <Link href="/">
+              <a href="/">
                 <Image src={logo} className="object-contain h-16 w-auto" alt='logo'/>
-              </Link>
+              </a>
             </div>
             <div className="flex items-center space-x-4 three-d">
                 {/* <a href="#home" className="text-white hover:text-blue-500 text-base">Home</a> */}
-                <Link href="/about">
-                <a href="#about" className="text-white hover:text-blue-500 text-base">About</a>
-                </Link>
-                <Link href="/portfolio">
-                <a href="#portfolio" className="text-white hover:text-blue-500 text-base">Portfolio</a>
-                </Link>
-                <Link href="/contact">
-                <a href="#contact" className="text-white hover:text-blue-500 text-base pr-6">Contact</a>
-                </Link>
+                <a href="/#about-me" className="text-white hover:text-blue-500 text-base">
+                About
+                </a>
+                <a href="/#projects" className="text-white hover:text-blue-500 text-base">
+                Portfolio
+                </a>
+                <a href="/#contact" className="text-white hover:text-blue-500 text-base pr-6">
+                Contact
+                </a>
             </div>
         </nav>
     </div>
