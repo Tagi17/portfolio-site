2b28d4a64a372ca4836984aa1aaf2056c19bb536
Nazia <naziaozair@gmail.com>
2024-03-11 22:17:16 -0400

    closer to achieving horizontal scroll

--- app/ProjectCard.tsx
+++ app/ProjectCard.tsx
@@ -17,97 +17,11 @@ if (typeof window !== "undefined") {
 }
 
 const ProjectCard = () => {
-  const infoList = useRef(null);
-  const imageList = useRef(null);
-
-  const progressWrap = gsap.utils.wrap(0, 1);
-
   useGSAP(() => {
-    gsap.set(".slide", { height: "0", opacity: '0' });
-
-    let sections = gsap.utils.toArray(".slide-item");
-
-    let scrollTween = gsap.to(sections, {
-      xPercent: -100 * (sections.length - 1),
-      ease: "none",
-      scrollTrigger: {
-        trigger: ".main-slide",
-        pin: "true",
-        scrub: 0.1,
-        end: "+=300",
-        onEnter: () => {
-          gsap.to(".slide", { height: "auto", opacity: 1, duration: 1});
-        }
-      }
-    })
-    gsap.to(".item-1", {
-      y: -130,
-      duration: 2,
-      ease: "elastic",
-      scrollTrigger: {
-        trigger: ".item-1",
-        containerAnimation: scrollTween,
-        start: "left center",
-        toggleActions: "play none none reset",
-        id: "1",
-      }      
-    });
-    // let sectionItems = document.querySelector(".main-slide") as HTMLElement;
-    // let tween: GSAPAnimation | undefined;
-    // let panels = gsap.utils.toArray(".main-slide .slide-item .item");
-    // const width = window.innerHeight * (panels.length - 1);
-    // const links = gsap.utils.toArray(".navTitle") as HTMLElement[];
-    // const modifiedLength = links.length - 1;
-    // let activeLink = 0;
-    // const snapPoints = links.map((_, i) => i / modifiedLength);
-    // const mySnap = gsap.utils.snap(snapPoints);
-    // links[activeLink].classList.toggle("active");
-
-    // function setupScrollTweens() {
-    //   tween = gsap.to(panels, {
-    //     xPercent: -50 * (panels.length - 1),
-    //     ease: "none",
-    //     scrollTrigger: {
-    //       trigger: ".main-slide",
-    //       pin: true,
-    //       scrub: 0.1,
-    //       end: `+=100`,
-    //       onUpdate: (self) => {
-    //         const newIndex = mySnap(self.progress) * modifiedLength;
-    //         if (newIndex !== activeLink) {
-    //           links[activeLink].classList.toggle("active");
-    //           links[newIndex].classList.toggle("active");
-    //           activeLink = newIndex;
-    //         }
-    //       }
-    //     }
-    //   });
-    // }
-    // setupScrollTweens();
-
-    // document.querySelectorAll(".navTitle").forEach((anchor) => {
-    //   anchor.addEventListener("click", function (e) {
-    //     e.preventDefault();
-    //     const target = e.currentTarget as HTMLElement;
-    //     const targetHref = target.getAttribute("href");
-    //     if (targetHref) {
-    //       let targetElem = document.querySelector(targetHref) as HTMLElement;
-    //       if (targetElem) {
-    //         if (tween && 'scrollTrigger' in tween && tween.scrollTrigger) {
-    //           const range = gsap.utils.mapRange(0, width, 0, tween.scrollTrigger.end);
-    //           const yPos = range(targetElem.offsetLeft);
-    //           gsap.to(window, {
-    //             scrollTo: {
-    //               y: yPos,
-    //               autoKill: false
-    //             },
-    //             duration: 0.5
-    //           });
-    //         }
-    //       }
-    //     }
-    //   });
-    // });
+    let sections = gsap.utils.toArray(".slideItem .item") as HTMLElement[];
+    let sectionsWidth = sections.map((section) => section.offsetWidth);
+    let totalSectionsWidth = sectionsWidth.reduce((acc, cur) => acc + cur, 0);
+    let amountToScroll = totalSectionsWidth - window.innerWidth;
 
     const items = gsap.utils.toArray(".item1 .content1") as HTMLElement[];
 
@@ -116,7 +30,7 @@ const ProjectCard = () => {
       window.innerHeight
     );
 
-    const tl = gsap.timeline({
+    const tlCards = gsap.timeline({
       scrollTrigger: {
         trigger: ".accordion-section1",
         start: "top 40%",
@@ -133,14 +47,34 @@ const ProjectCard = () => {
 
     items.forEach((item1, i) => {
       if (items[i + 1]) {
-        tl.to(item1, { height: 0 }).to(items[i + 1], { height: "auto" }, "<");
+        tlCards
+          .to(item1, { height: 0 })
+          .to(items[i + 1], { height: "auto" }, "<");
       }
     });
+    gsap.set(".slideItem", { height: "0", opacity: "0" });
+    
+    sections.forEach((slideItem, i) => {
+      if (sections[i + 1]) {
+        const st = gsap.timeline()
 
+        ScrollTrigger.create({
+          animation: st,
+          trigger: ".container",
+          containerAnimation: tlCards.pin,
+          scrub: 1,
+          end: 'center center',
+        });
+ 
+        st.to(slideItem, { x: -100 }).to(sections[i + 1], { x: -100, height: "auto", opacity: "auto" },"<" );
+      }
+    });
+  
+    
     gsap.set(".wrapper1", { autoAlpha: 1 });
 
     return () => {
-      tl.kill();
+      tlCards.kill();
     };
   });
 
@@ -158,21 +92,19 @@ const ProjectCard = () => {
               <div className="three-d text-5xl font-bold p-3">About Me</div>
             </div>
             <div className="content1 three-d text-3xl px-14 py-9">
-              <div className="">
-                Fuelled by a passion for crafting impactful products, my journey
-                into programming began with Girls Who Code—an experience that
-                sparked my curiosity in tech. It wasn&apos;t until I encountered
-                Bitcoin that I found my true calling. The technology behind it
-                not only captivated me but also unveiled a realm of exciting
-                possibilities. Now, I&apos;m integrating blockchain into my
-                projects and I leverage my design skills to enhance user
-                experiences in innovative ways.
-              </div>
+              Fuelled by a passion for crafting impactful products, my journey
+              into programming began with Girls Who Code—an experience that
+              sparked my curiosity in tech. It wasn&apos;t until I encountered
+              Bitcoin that I found my true calling. The technology behind it not
+              only captivated me but also unveiled a realm of exciting
+              possibilities. Now, I&apos;m integrating blockchain into my
+              projects and I leverage my design skills to enhance user
+              experiences in innovative ways.
               <div className="p-5"></div>
             </div>
           </div>
           <div
-            className="item1 mb-12 border-2 border-primary-300 bg-primary-500 rounded-lg shadow-xl overflow-hidden"
+            className="item1 container mb-12 border-2 border-primary-300 bg-primary-500 rounded-lg shadow-xl overflow-hidden"
             id="projects"
             data-label="label1"
           >
@@ -189,27 +121,27 @@ const ProjectCard = () => {
                 <div className="slides-container">
                   <div className="navTitle-wrap">
                     <div className="sections-inner">
-                      <div className="slide-item link-1">
+                      <div className=" link-1">
                         <div className="section2 navTitle" id="button1"></div>
                         <a href="#item1" className="section2 navTitle"></a>
                       </div>
-                      <div className="slide-item link-2">
+                      <div className=" link-2">
                         <div className="section2 navTitle" id="button2"></div>
                         <a href="#item2" className="section2 navTitle"></a>
                       </div>
-                      <div className="slide-item link-3">
+                      <div className=" link-3">
                         <div className="section2 navTitle" id="button3"></div>
                         <a href="#item3" className="section2 navTitle"></a>
                       </div>
-                      <div className="slide-item link-4">
+                      <div className=" link-4">
                         <div className="section2 navTitle" id="button4"></div>
                         <a href="#item4" className="section2 navTitle"></a>
                       </div>
                     </div>
                   </div>
-                  </div>
-                  <div className="slide">
-                  <div className="slide-item item1" id="item1">
+                </div>
+                <div className="slide">
+                  <div className="slideItem item1" id="item1">
                     <div className="item item-1">
                       <div className="objects">Relay Project is about</div>
                       <div className="image">
@@ -221,73 +153,73 @@ const ProjectCard = () => {
                         />
                       </div>
                     </div>
-                    <div className="slide-item item2" id="item2">
-                      <div className="item item-2">
-                        <div className="objects">Relay Project is about</div>
-                        <div className="image">
-                          <Image
-                            src={defi}
-                            height={50}
-                            width={50}
-                            alt={"image"}
-                          />
-                        </div>
+                  </div>
+                  <div className="slideItem item2" id="item2">
+                    <div className="item item-2">
+                      <div className="objects">Relay Project is about</div>
+                      <div className="image">
+                        <Image
+                          src={defi}
+                          height={50}
+                          width={50}
+                          alt={"image"}
+                        />
                       </div>
                     </div>
-                    <div className="slide-item item3" id="item3">
-                      <div className="item item-3">
-                        <div className="objects">Relay Project is about</div>
-                        <div className="image">
-                          <Image
-                            src={defi}
-                            height={50}
-                            width={50}
-                            alt={"image"}
-                          />
-                        </div>
+                  </div>
+                  <div className="slideItem item3" id="item3">
+                    <div className="item item-3">
+                      <div className="objects">Relay Project is about</div>
+                      <div className="image">
+                        <Image
+                          src={defi}
+                          height={50}
+                          width={50}
+                          alt={"image"}
+                        />
                       </div>
                     </div>
-                    <div className="slide-item item4" id="item4">
-                      <div className="item item-4">
-                        <div className="objects">Relay Project is about</div>
-                        <div className="image">
-                          <Image
-                            src={defi}
-                            height={50}
-                            width={50}
-                            alt={"image"}
-                          />
-                        </div>
+                  </div>
+                  <div className="slideItem item4" id="item4">
+                    <div className="item item-4">
+                      <div className="objects">Relay Project is about</div>
+                      <div className="image">
+                        <Image
+                          src={defi}
+                          height={50}
+                          width={50}
+                          alt={"image"}
+                        />
                       </div>
+                    </div>
                   </div>
                 </div>
-              </div>
                 <div className="p-1"></div>
               </div>
             </div>
           </div>
-            <div
-              className="item1 mb-12 border-2 border-primary-300 bg-primary-500 rounded-lg shadow-xl overflow-hidden"
-              id="contact"
-              data-label="label2"
-            >
-              <div className="header1 bg-primary-600 text-white text-xl font-bold p-4 cursor-pointer">
-                <div className="three-d text-5xl font-bold p-2">Boop</div>
-              </div>
-              <div className="content1 three-d text-2xl px-8 py-8">
-                <div>
-                  Libero assumenda iste, tempora eos facilis consectetur cum
-                  esse officia magnam repudiandae mollitia itaque voluptates
-                  corporis eveniet perferendis saepe hic aliquid veritatis ipsa
-                  similique voluptatibus nisi harum. Voluptates blanditiis nam,
-                  aliquid distinctio repellendus molestiae numquam doloremque
-                  totam impedit. Recusandae laborum debitis quidem assumenda
-                  laudantium perferendis reprehenderit repellendus quos, labore
-                  ratione, laboriosam autem!
-                </div>
-                <div className="p-5"></div>
-              </div>
+          <div
+            className="item1 mb-12 border-2 border-primary-300 bg-primary-500 rounded-lg shadow-xl overflow-hidden"
+            id="contact"
+            data-label="label2"
+          >
+            <div className="header1 bg-primary-600 text-white text-xl font-bold p-4 cursor-pointer">
+              <div className="three-d text-5xl font-bold p-2">Boop</div>
             </div>
+            <div className="content1 three-d text-2xl px-8 py-8">
+              <div>
+                Libero assumenda iste, tempora eos facilis consectetur cum esse
+                officia magnam repudiandae mollitia itaque voluptates corporis
+                eveniet perferendis saepe hic aliquid veritatis ipsa similique
+                voluptatibus nisi harum. Voluptates blanditiis nam, aliquid
+                distinctio repellendus molestiae numquam doloremque totam
+                impedit. Recusandae laborum debitis quidem assumenda laudantium
+                perferendis reprehenderit repellendus quos, labore ratione,
+                laboriosam autem!
+              </div>
+              <div className="p-5"></div>
+            </div>
+          </div>
         </div>
         {/* </Parallax> */}
       </div>
